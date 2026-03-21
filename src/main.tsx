124bcc759b36feae8ae72501514100adeee85e35
JeonSuna <jsa020910@gmail.com>
2026-03-21 15:19:23 +0900

    chore: Devtools 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/app/styles/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "@/app/providers/ThemProvider.tsx";
 import { HelmetProvider } from "react-helmet-async";
 import App from "@/app/App";
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
           <App />
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
